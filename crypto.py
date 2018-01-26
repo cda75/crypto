00ffc01c6a99a3d98e7a96d2ca4bd5104cf817aa
cda75 <dchestnov@mail.ru>
2018-01-26 11:28:27 +0300

    ss

--- crypto.py
+++ crypto.py
@@ -57,6 +57,14 @@ def check_pid(pid):
 		return False
 
 
+def set_env():
+	os.system('setx GPU_FORCE_64BIT_PTR 0')
+	os.system('setx GPU_MAX_HEAP_SIZE 100')
+	os.system('setx GPU_USE_SYNC_OBJECTS 1')
+	os.system('setx GPU_MAX_ALLOC_PERCENT 100')
+	os.system('setx GPU_SINGLE_ALLOC_PERCENT 100')
+
+
 def get_best_coin():
 	JSON_URL = r"https://whattomine.com/coins.json?utf8=✓&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=3&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=1&adapt_10606=true&adapt_q_1070=2&adapt_1070=true&adapt_q_1070Ti=1&adapt_1070Ti=true&adapt_q_1080=03&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=113.0&factor%5Beth_p%5D=465.0&grof=true&factor%5Bgro_hr%5D=124.0&factor%5Bgro_p%5D=470.0&x11gf=true&factor%5Bx11g_hr%5D=43.4&factor%5Bx11g_p%5D=450.0&cn=true&factor%5Bcn_hr%5D=2320.0&factor%5Bcn_p%5D=360.0&eq=true&factor%5Beq_hr%5D=1600.0&factor%5Beq_p%5D=450.0&lre=true&factor%5Blrev2_hr%5D=132300.0&factor%5Blrev2_p%5D=470.0&ns=true&factor%5Bns_hr%5D=3550.0&factor%5Bns_p%5D=470.0&lbry=true&factor%5Blbry_hr%5D=1020.0&factor%5Blbry_p%5D=450.0&factor%5Bbk2b_hr%5D=5990.0&factor%5Bbk2b_p%5D=440.0&factor%5Bbk14_hr%5D=9100.0&factor%5Bbk14_p%5D=460.0&pas=true&factor%5Bpas_hr%5D=3580.0&factor%5Bpas_p%5D=450.0&skh=true&factor%5Bskh_hr%5D=104.5&factor%5Bskh_p%5D=450.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate"	
 	cfg = SafeConfigParser()
@@ -106,7 +114,8 @@ def start_coin_mining(coin, algo):
 		dworker = cfg.get('ETH', 'DWORKER')
 		dpassword = cfg.get('ETH', 'DPASSWORD')
 		dcoin = cfg.get('ETH', 'DCOIN')
-		cmdStr = "%s -epool %s:%s -ewal %s.%s -epsw %s -allcoins 1 -allpools 1 -dpool %s:%s -dwal %s.%s -dpsw %s -dcoin %s" %(miner_bin, pool, port, user, worker, password, dpool, dport, duser, dworker, dpassword, dcoin)
+		set_env()
+		cmdStr = "%s -di 023 -epool %s:%s -ewal %s.%s -epsw %s -allcoins 1 -allpools 1 -dpool %s:%s -dwal %s.%s -dpsw %s -dcoin %s" %(miner_bin, pool, port, user, worker, password, dpool, dport, duser, dworker, dpassword, dcoin)
 	else:
 		# CCMINER
 		cmdStr = "%s -a %s -o %s:%s -u %s.%s --cpu-priority=3" %(miner_bin, algo, pool, port, user, worker)
