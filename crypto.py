5e50dfd8aae133d18e5b029a872822230213727a
cda75 <dchestnov@mail.ru>
2018-01-29 00:11:45 +0300

    aaa

--- crypto.py
+++ crypto.py
@@ -65,11 +65,14 @@ def set_env():
 	os.system('setx GPU_SINGLE_ALLOC_PERCENT 100')
 
 
-def get_best_coin():
+def get_best_coin(coins=None):
 	JSON_URL = r"https://whattomine.com/coins.json?utf8=✓&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=3&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=1&adapt_10606=true&adapt_q_1070=2&adapt_1070=true&adapt_q_1070Ti=1&adapt_1070Ti=true&adapt_q_1080=03&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=113.0&factor%5Beth_p%5D=465.0&grof=true&factor%5Bgro_hr%5D=124.0&factor%5Bgro_p%5D=470.0&x11gf=true&factor%5Bx11g_hr%5D=43.4&factor%5Bx11g_p%5D=450.0&cn=true&factor%5Bcn_hr%5D=2320.0&factor%5Bcn_p%5D=360.0&eq=true&factor%5Beq_hr%5D=1600.0&factor%5Beq_p%5D=450.0&lre=true&factor%5Blrev2_hr%5D=132300.0&factor%5Blrev2_p%5D=470.0&ns=true&factor%5Bns_hr%5D=3550.0&factor%5Bns_p%5D=470.0&lbry=true&factor%5Blbry_hr%5D=1020.0&factor%5Blbry_p%5D=450.0&factor%5Bbk2b_hr%5D=5990.0&factor%5Bbk2b_p%5D=440.0&factor%5Bbk14_hr%5D=9100.0&factor%5Bbk14_p%5D=460.0&pas=true&factor%5Bpas_hr%5D=3580.0&factor%5Bpas_p%5D=450.0&skh=true&factor%5Bskh_hr%5D=104.5&factor%5Bskh_p%5D=450.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate"	
-	cfg = SafeConfigParser()
-	cfg.read(COINS)
-	MY_COINS = cfg.sections()
+	if coins:
+		MY_COINS = [x.strip() for x in coins.split(',')]
+	else:
+		cfg = SafeConfigParser()
+		cfg.read(COINS)
+		MY_COINS = cfg.sections()
 	try:
 		logging("[i] Checking best coin...")
 		req = requests.get(JSON_URL)
@@ -131,14 +134,14 @@ def start_coin_mining(coin):
 		exit()
 
 
-def coin_mining(t1=30, t2=12):
+def coin_mining(t1=30, t2=12, coins='all'):
 	logging("[i] Started coin mining")
-	coin = get_best_coin()
+	coin = get_best_coin(coins)
 	logging("[i] My current most profitable coin is %s" %coin)
 	start_coin_mining(coin)
 	for i in range(int(60/t1*t2)):
 		sleep(t1*60)
-		new_coin = get_best_coin()
+		new_coin = get_best_coin(coins)
 		if new_coin == coin:
 			logging("[+] Continue mining %s\n" %coin)
 		else:
@@ -219,7 +222,10 @@ def nicehash_mining(t1=2, t2=12):
 	best_algo = nicehash_best_algo()
 	logging("[i] Current NiceHash best algo: %s" %(best_algo['name']))
 	start_nicehash_mining(best_algo)
-	for i in range(int(60/t1*t2)):
+	if t1 == 0:
+		exit()
+	cycles = int(60/t1*t2)
+	for i in range(cycles):
 		sleep(t1*60)
 		new_algo = nicehash_best_algo()
 		if new_algo['name'] == best_algo['name']:
@@ -275,8 +281,9 @@ def mine_eth(coin='ETH'):
 
 if __name__ == "__main__":
 	while True:
-		coin_mining(t2=24)
-		nicehash_mining(t2=24)
+		coin_mining()
+		nicehash_mining()
+
 
 
 
