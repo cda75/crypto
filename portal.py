2fd8d7cc816bd77c818920316257945a2b3316f0
cda75 <dchestnov@mail.ru>
2018-02-06 18:37:19 +0300

    asd

--- portal.py
+++ portal.py
@@ -16,7 +16,7 @@ import psutil
 
 WORK_DIR = os.path.dirname(os.path.realpath(__file__))
 COINS = os.path.join(WORK_DIR, 'coins.conf')
-API = os.path.join(WORK_DIR, 'api.conf')
+#API = os.path.join(WORK_DIR, 'api.conf')
 PRICES = os.path.join(WORK_DIR, 'price.csv')
 BALANCE = os.path.join(WORK_DIR, 'balance.csv')
 PID = os.path.join(WORK_DIR, 'PID')
@@ -25,19 +25,6 @@ COIN = os.path.join(WORK_DIR, 'COIN')
 MY_COINS = ['BTC','ETH','ETC','ZEC','XVG','ZCL','XMR','KMD','HUSH']
 
 app = Flask(__name__)
-
-ALGO = {'ETH': 'Ethash',
-		'ETC': 'Ethash',
-		'ZEC': 'Equihash',
-		'ZCL': 'Equihash',
-		'KMD': 'Equihash',
-		'BTG': 'Equihash',
-		'ZEN': 'Equihash',
-		'HUSH': 'Equihash',
-		'XVG': 'X17',
-		'XMR': 'Cryptonight'
-}
-
 
 @app.route('/')
 def index():
@@ -48,7 +35,6 @@ def index():
 def main():
 	stat = dict()
 	stat['coin'] = get_current_coin()
-	#stat['algo'] = ALGO[stat['coin']]
 	proc_name = get_process_name()
 	stat['process_name'] = proc_name
 	stat['uptime'] = get_process_uptime(proc_name)
@@ -59,7 +45,6 @@ def main():
 def market():
 	prices = get_market_prices()
 	return render_template('market.html', data=prices)
-
 
 
 @app.route('/balance.html')
@@ -111,35 +96,6 @@ def get_process_uptime(process_name):
 
 
 def get_coin_balance(coin):
-	if coin in ['ZEC','BTC','ETH','ETC','XVG']:
-		requests.packages.urllib3.disable_warnings()
-		cfg = SafeConfigParser()
-		cfg.read(API)
-		url = cfg.get(coin, 'BALANCE')
-		req = requests.get(url)
-		if coin == "BTC":
-			value = float(req.json())/10**8
-		elif coin == 'ZEC':
-			value = float(req.json()['balance'])
-		elif coin in ["ETH", "ETC"]:
-			value = float(req.json()['balance'])/10**18
-		elif coin == 'XVG':
-			req = requests.get(url, verify=False)
-			value = float(req.json())
-		return value
-	with open(BALANCE) as f:
-		reader = csv.reader(f)
-		value = dict()
-		for row in reader:
-			if row[0] == coin:
-				return float(row[1])
-
-
-def write_balance_to_csv():
-	pass
-
-
-def get_coin_balance_2(coin):
 	with open(BALANCE) as f:
 		reader = csv.reader(f)
 		value = dict()
@@ -161,12 +117,48 @@ def get_market_prices():
 	return sorted(rez, key=itemgetter(0))
 
 
-
 def get_coin_price(coin):
 	prices = get_market_prices()
 	for price in prices:
 		if coin == price[0]:
 			return price[1], price[2], price[3]
+
+
+class BalanceData(object):
+	def __init__(self, interval=3600):
+		self.interval = interval
+		self.COINS = ['ZEC','BTC','ETH','XVG']
+		self.API = os.path.join(WORK_DIR, 'api.conf')
+		self.BALANCE = os.path.join(WORK_DIR, 'balance.csv')
+		thread = threading.Thread(target=self.run, args=())
+		thread.daemon = True                         
+		thread.start()
+	def run(self):
+		requests.packages.urllib3.disable_warnings()
+		cfg = SafeConfigParser()
+		cfg.read(self.API)
+		with open(self.BALANCE) as f:
+			lines = list(csv.reader(f))
+		for coin in self.COINS:
+			url = cfg.get(coin, 'BALANCE')
+			req = requests.get(url, verify=False)
+			if coin == "BTC":
+				value = float(req.json())/10**8
+			elif coin == 'ZEC':
+				value = float(req.json()['balance'])
+			elif coin in ["ETH", "ETC"]:
+				value = float(req.json()['balance'])/10**18
+			elif coin == 'XVG':
+				value = float(req.json())		
+			for line in lines:
+				if line[0] == coin:
+					line[1] = value
+		with open(BALANCE, 'wb') as f:
+			writer = csv.writer(f)
+			writer.writerows(lines)
+		sleep(self.interval)
+
+		
 
 
 '''
@@ -203,11 +195,9 @@ class MarketData(object):
 
 
 if __name__ == "__main__":
-	#MarketData()
-	#app.run(host="0.0.0.0", debug = True)
-	proc = get_process_name()
-	print proc
-	print get_process_uptime(proc)
+	BalanceData(interval=60)
+	app.run(host="0.0.0.0", debug = True)
+
 
 
 
