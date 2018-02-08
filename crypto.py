91072d209d6e78f0818d39f12ca927c4444df79c
cda75 <dchestnov@mail.ru>
2018-02-08 12:26:23 +0300

    add address to Niceshash miner

--- crypto.py
+++ crypto.py
@@ -290,8 +290,8 @@ def nicehash_mining(t1=2, t2=12, ADDR=''):
 
 if __name__ == "__main__":
 	while True:	
-		nicehash_mining(t2=1, ADDR='1P2EP9MbWMFtRw5Ns7kv5LHcofn2nmHFdV')
-		coin_mining(t2=10, coins='ETH, ETC, XVG, KMD, HASH, ZCL, ZEC')
-		nicehash_mining(t2=1, ADDR='1B1oMEyYkA7fPh8M5EWCnevUXWsATN33Vv')
+		nicehash_mining(t2=2, ADDR='1P2EP9MbWMFtRw5Ns7kv5LHcofn2nmHFdV')
+		coin_mining(t2=9, coins='ETH, ETC, XVG, KMD, HASH, ZCL, ZEC')
+		nicehash_mining(t2=0.5, ADDR='1B1oMEyYkA7fPh8M5EWCnevUXWsATN33Vv')
 		coin_mining(t1=0, t2=0.5, coins='XVG')
 		
