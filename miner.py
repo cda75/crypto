e14edbf4fad692235c6d941632337657319d1370
cda75 <dchestnov@mail.ru>
2018-02-12 18:09:07 +0300

    dfg

--- miner.py
+++ miner.py
@@ -24,10 +24,7 @@ class Miner(object):
 	def __init__(self, coin='ZEC', log=False):
 		self.__set_parameters(coin)
 		self.log = log
-		self.__equihash_bin = 'dtsm'	
-		#thread = threading.Thread(target=self.run, args=())
-		#thread.daemon = True                         
-		#thread.start()
+		self.__equihash_bin = 'dtsm'
 
 	def __set_parameters(self, coin):
 		self.__coin = coin
@@ -42,10 +39,13 @@ class Miner(object):
 		self.__password = cfg.get(coin, 'PASSWORD')
 		cfg.read(CONFIG)
 		self.__bin = cfg.get('ALGO', self.__algo)
-		self.__pid = os.path.basename(self.__bin)
+		self.__pid = [os.path.basename(self.__bin)]
 
 	def set_coin(self, coin):
 		self.__set_parameters(coin)
+
+	def set_gpu(self, gpu):
+		self.__gpu = gpu
 
 	def get_coin(self):
 		return self.__coin
@@ -80,36 +80,39 @@ class Miner(object):
 			with open(LOG, 'a') as f:
 				f.write(time+info+'\n')	
 		
-	def start(self):	
+	def start(self):
+		cmdStr = []
 		if self.__algo == 'equihash':
 			if self.__equihash_bin == 'ewbf':
-				cmdStr = "%s --server %s --port %s --user %s.%s --api 0.0.0.0:42000 --fee 0" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker)
+				cmdStr.append("%s --server %s --port %s --user %s.%s --api 0.0.0.0:42000 --fee 0" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
 			else:
-				cmdStr = "%s --server %s --port %s --user %s.%s --telemetry=0.0.0.0:42000" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker)
+				cmdStr.append("%s --server %s --port %s --user %s.%s --telemetry=0.0.0.0:42000" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
 		elif self.__algo == 'ethash':
-			cmdStr = "%s -di 023 -epool %s:%s -ewal %s.%s " %(self.__bin, self.__pool, self.__port, self.__user, self.__worker)
+			cmdStr.append("%s -di 023 -epool %s:%s -ewal %s.%s " %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
+			self.__set_parameters('ZEC')
+			if self.__equihash_bin == 'ewbf':
+				cmdStr.append("%s --server %s --cuda_devices 1 --port %s --user %s.%s --api 0.0.0.0:42000 --fee 0" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
+			else:
+				cmdStr.append("%s --server %s --dev 1 --port %s --user %s.%s --telemetry=0.0.0.0:42000" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
+			self.__coin = "ETH ZEC"
+			self.__pid.append('ZEC')
 		else:
-			cmdStr = "%s -a %s -o %s:%s -u %s.%s --cpu-priority=3" %(self.__bin, self.__algo, self.__pool, self.__port, self.__user, self.__worker)
+			cmdStr.append("%s -a %s -o %s:%s -u %s.%s --cpu-priority=3" %(self.__bin, self.__algo, self.__pool, self.__port, self.__user, self.__worker))
 		try:
-			Popen(cmdStr, creationflags=CREATE_NEW_CONSOLE)
-			with open(PID, 'w') as f:
-				f.write(self.__pid)
-			with open(COIN, 'w') as f:
-				f.write(self.__coin)
+			for cmd in cmdStr:
+				Popen(cmd, creationflags=CREATE_NEW_CONSOLE)
 			self.__logging("[+] Successfully started %s mining\n" %self.__coin)
 		except:
 			self.__logging("[-] ERROR started %s mining\nExit\n" %self.__coin)
 			exit()
 
-
 	def stop(self):
-		cmdStr = "taskkill /f /im %s" %(self.__pid)
-		try:	
-			os.system(cmdStr)
-		except:
-			return False
-
-
+		for pid in self.__pid:
+			cmdStr = "taskkill /f /im %s" %(pid)
+			try:	
+				os.system(cmdStr)
+			except:
+				self.__logging("[-] Error stopping process\n" %pid)
 
 	def restart(self):
 		pass
