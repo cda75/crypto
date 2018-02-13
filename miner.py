5ff5b5bee0c07810d7dc659b89e104f3de4628d4
cda75 <dchestnov@mail.ru>
2018-02-13 16:27:47 +0300

    fgh

--- miner.py
+++ miner.py
@@ -42,9 +42,6 @@ class Miner(object):
 	def set_coin(self, coin):
 		self.__set_parameters(coin)
 
-	def set_gpu(self, gpu):
-		self.__gpu = gpu
-
 	def get_coin(self):
 		return self.__coin
 
@@ -84,10 +81,8 @@ class Miner(object):
 				f.write(pid+'\n')
 
 	def __write_coin(self):
-		coins = self.__coin.split()
 		with open(COIN, 'w') as f:
-			for coin in coins:
-				f.write(coin+'\n')
+			f.write(self.__coin)
 
 	def start(self):
 		cmdStr = []
@@ -104,7 +99,6 @@ class Miner(object):
 				cmdStr.append("%s --server %s --cuda_devices 1 --port %s --user %s.%s --api 0.0.0.0:42000 --fee 0" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
 			else:
 				cmdStr.append("%s --server %s --dev 1 --port %s --user %s.%s --telemetry=0.0.0.0:42000" %(self.__bin, self.__pool, self.__port, self.__user, self.__worker))
-			self.__coin = "ETH ZEC"
 			self.__pid.append(eth_pid)
 		else:
 			cmdStr.append("%s -a %s -o %s:%s -u %s.%s --cpu-priority=3" %(self.__bin, self.__algo, self.__pool, self.__port, self.__user, self.__worker))
@@ -132,7 +126,7 @@ class Miner(object):
 				self.__logging("[-] Error stoping process\n" %pid)
 
 	def restart(self):
-		coin = self.__coin.split()[0]
+		coin = self.__coin
 		self.stop()
 		self.__set_parameters(coin)
 		self.start()
@@ -152,15 +146,60 @@ class Miner(object):
 		thread = threading.Thread(target=check_thread)   
 		thread.daemon = True                     
 		thread.start()
+
+
+
+def get_best_coin(coins='all', algo='all'):
+	JSON_URL = r"https://whattomine.com/coins.json?utf8=✓&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=3&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=1&adapt_10606=true&adapt_q_1070=2&adapt_1070=true&adapt_q_1070Ti=1&adapt_1070Ti=true&adapt_q_1080=03&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=113.0&factor%5Beth_p%5D=465.0&grof=true&factor%5Bgro_hr%5D=124.0&factor%5Bgro_p%5D=470.0&x11gf=true&factor%5Bx11g_hr%5D=43.4&factor%5Bx11g_p%5D=450.0&cn=true&factor%5Bcn_hr%5D=2320.0&factor%5Bcn_p%5D=360.0&eq=true&factor%5Beq_hr%5D=1600.0&factor%5Beq_p%5D=450.0&lre=true&factor%5Blrev2_hr%5D=132300.0&factor%5Blrev2_p%5D=470.0&ns=true&factor%5Bns_hr%5D=3550.0&factor%5Bns_p%5D=470.0&lbry=true&factor%5Blbry_hr%5D=1020.0&factor%5Blbry_p%5D=450.0&factor%5Bbk2b_hr%5D=5990.0&factor%5Bbk2b_p%5D=440.0&factor%5Bbk14_hr%5D=9100.0&factor%5Bbk14_p%5D=460.0&pas=true&factor%5Bpas_hr%5D=3580.0&factor%5Bpas_p%5D=450.0&skh=true&factor%5Bskh_hr%5D=104.5&factor%5Bskh_p%5D=450.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate"	
+	if coins != 'all':
+		MY_COINS = [x.strip() for x in coins.split(',')]
+	else:
+		cfg = SafeConfigParser()
+		cfg.read(COINS)
+		MY_COINS = cfg.sections()
+	if algo != 'all':
+		MY_ALGO = algo
+	else:
+		cfg = SafeConfigParser()
+		cfg.read(BENCHMARK)
+		algo_dict = dict(cfg.items('DEFAULT'))
+		MY_ALGO = algo_dict.keys()
+	try:
+		logging("[i] Checking best coin...")
+		req = requests.get(JSON_URL)
+		reqResult = req.json()['coins']
+		best_dict = {}
+		for value in reqResult.values():
+			if value['tag'] in MY_COINS:
+				best_dict[value['tag']] = value['profitability']
+		rez = sorted(best_dict.items(), key=operator.itemgetter(1), reverse=True)
+		return rez[0][0]
+	except:
+		logging("[-] Error getting data from WhatToMine....Mining default coin")
+		return 'ZEC'
+
+
+def coin_mining(coin, check_time=0.5):
+	m = Miner()
+	m.check()
+	best_coin = ''
+	if coin:
+		m.set_coin(coin)
+		m.start()
+	else:
+		best_coin = get_best_coin()
+		m.set_coin(best_coin)
+		m.start()
+		while True:
+			sleep(check_time*3600)
+			best_coin = get_best_coin()
+			if best_coin != m.get_coin():
+				m.set_coin(best_coin)
+				m.restart()
+			
 		
 
 if __name__ == "__main__":
-	coins = ['ZCL', 'ZEC', 'ETH', 'XVG']
-	m = Miner()
-	m.check()
-	while True:
-		for coin in coins:
-			m.set_coin(coin)
-			m.start()
-			sleep(1800)
-			m.stop()
+	coin_mining()
+	
+	
